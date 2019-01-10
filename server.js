9492a351c1475c174da68c8058ec45cc25494213
unknown <vponjavic@gmail.com>
2019-01-10 19:03:18 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs', {
+//     pageTitle: 'Maintenance'
+//   });
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Zdravo svete!</h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Houmpejdž',
+    welcomeMessage: 'Dobrodošli u matriks!'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'O nama'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMesage: 'Unable to access page'
+  });
+})
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000')
+});
